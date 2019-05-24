277791075e9ec81cdfd63233be0c4aed024dd28e
evan999 <emeshberge@gmail.com>
2019-05-23 21:33:08 -0400

    Define first test.

---
+++ UnitTests/StringUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class StringUtils
+    {
+        public int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            // TODO: Implement this method
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/StringUtilsTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture]
+
+    public class StringUtilsTest
+    {
+        [Test]
+
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+            var sentenceToScan = "TDD is awesome!";
+            var characterToScanFor = "e";
+            var expectedResult = 2;
+
+            var stringUtils = new StringUtils();
+
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+
+
+        }
+    }
+}
